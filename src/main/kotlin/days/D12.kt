502e82e0a8efa8397c6f92ce1e31202824f1f43d
Jasper den Brok <jasper.den.brok@gmail.com>
2023-12-12 10:28:36 +0100

    Refactor code

--- src/main/kotlin/days/D12.kt
+++ src/main/kotlin/days/D12.kt
@@ -5,23 +5,17 @@ object D12 : Day {
     private val input = readFile().map { line -> line.split(" ").let { (l, r) -> l to r } }
 
     private fun getArrangements(s: String, ls: List<Int>): Long {
-        val rangeDp = List(s.length) { i ->
-            BooleanArray(s.length - i) { j ->
-                (i .. i + j).none { k -> s[k] == '.' }
-            }
-        }
-        fun rangeDp(i: Int, l: Int): Boolean =
-            rangeDp[i].let { arr -> l <= arr.size && arr[l - 1] && (i + l == s.length || s[i + l] != '#') }
-
+        val memo = IntArray(s.length) { i -> s.drop(i).takeWhile { c -> c != '.' }.length }
         val dp = mutableMapOf<Pair<Int, Int>, Long>()
 
+        fun canTake(i: Int, l: Int) = memo[i] >= l && (i + l == s.length || s[i + l] != '#')
         fun helper(si: Int, lsi: Int): Long =
             when {
-                lsi == ls.size -> if (si >= s.length || s.substring(si).none { c -> c == '#' }) 1L else 0
+                lsi == ls.size -> if (s.drop(si).none { c -> c == '#' }) 1L else 0
                 si >= s.length -> 0L
                 else -> {
                     if (dp[si to lsi] == null) {
-                        val take = if (rangeDp(si, ls[lsi])) helper(si + ls[lsi] + 1, lsi + 1) else 0L
+                        val take = if (canTake(si, ls[lsi])) helper(si + ls[lsi] + 1, lsi + 1) else 0L
                         val dontTake = if (s[si] != '#') helper(si + 1, lsi) else 0L
                         dp[si to lsi] = take + dontTake
                     }
