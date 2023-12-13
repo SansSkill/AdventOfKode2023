34589ec32be9da3614dd2d41399a9ba4f4748688
Jasper den Brok <jasper.den.brok@gmail.com>
2023-12-13 13:32:28 +0100

    Improve D05 to run in ~4s instead of ~88s

--- src/main/kotlin/days/D05.kt
+++ src/main/kotlin/days/D05.kt
@@ -1,46 +1,41 @@
 package days
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.withContext
-
 @Suppress("unused")
 object D05 : Day {
     private val seeds: List<Long>
     private val seedRanges: List<LongRange>
-    private val seedToSoilMap: List<Pair<LongRange, Long>>
-    private val soilToFertilizerMap: List<Pair<LongRange, Long>>
-    private val fertilizerToWaterMap: List<Pair<LongRange, Long>>
-    private val waterToLightMap: List<Pair<LongRange, Long>>
-    private val lightToTemperatureMap: List<Pair<LongRange, Long>>
-    private val temperatureToHumidityMap: List<Pair<LongRange, Long>>
-    private val humidityToLocationMap: List<Pair<LongRange, Long>>
+    private val seedToLocationMapList: List<List<Pair<LongRange, Long>>>
+    private val locationToSeedMapList: List<List<Pair<LongRange, Long>>>
 
     init {
         val input = readFile()
-        fun toPair(line: String): Pair<LongRange, Long> = line.split(" ").map(String::toLong).let { (n1, n2, n3) -> n2 ..< n2 + n3 to n1 }
         seeds = Regex("\\d+").findAll(input[0]).map(MatchResult::value).map(String::toLong).toList()
         seedRanges = (seeds.indices step 2).map { i -> seeds[i]..<seeds[i] + seeds[i + 1] }
-        var dropOffset = 3
-        fun getMap() = input.drop(dropOffset).takeWhile(String::isNotEmpty).map(::toPair).sortedBy { (range, _) -> range.first }.also { map -> dropOffset += map.size + 2 }
-        seedToSoilMap = getMap()
-        soilToFertilizerMap = getMap()
-        fertilizerToWaterMap = getMap()
-        waterToLightMap = getMap()
-        lightToTemperatureMap = getMap()
-        temperatureToHumidityMap = getMap()
-        humidityToLocationMap = getMap()
+        fun getMapList(toPair: (String) -> (Pair<LongRange, Long>)): List<List<Pair<LongRange, Long>>> {
+            var dropOffset = 3
+            return (0..6).map {
+                input.drop(dropOffset).takeWhile(String::isNotEmpty)
+                    .map { line -> toPair(line) }.sortedBy { (range, _) -> range.first }
+                    .also { map -> dropOffset += map.size + 2 }
+            }
+        }
+        seedToLocationMapList = getMapList { line ->
+            line.split(" ").map(String::toLong).let { (n1, n2, n3) -> n2 ..< n2 + n3 to n1 }
+        }
+        locationToSeedMapList = getMapList { line ->
+            line.split(" ").map(String::toLong).let { (n1, n2, n3) -> n1 ..< n1 + n3 to n2 }
+        }.reversed()
     }
 
-    private fun getLocation(seed: Long): Long =
-        listOf(seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap)
-            .fold(seed) { pos, map -> map.firstOrNull { (range) -> pos in range }?.let { (range, m) -> m + pos - range.first } ?: pos }
+    private fun List<List<Pair<LongRange, Long>>>.fold(start: Long): Long =
+        fold(start) { pos, map -> map.firstOrNull { (range) -> pos in range }?.let { (range, m) -> m + pos - range.first } ?: pos }
 
     override suspend fun part1(): String =
-        seeds.minOf(::getLocation).toString()
+        seeds.minOf { seed -> seedToLocationMapList.fold(seed) }.toString()
 
-    override suspend fun part2(): String = withContext(Dispatchers.IO) {
-        seedRanges.map { range -> async { range.minOf(::getLocation) } }.awaitAll().min().toString()
-    }
+    override suspend fun part2(): String =
+        generateSequence(0L, Long::inc).first { location ->
+            val seed = locationToSeedMapList.fold(location)
+            seedRanges.any { range -> seed in range }
+        }.toString()
 }
